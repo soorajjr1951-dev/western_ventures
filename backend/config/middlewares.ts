4a13f73b9a1b7f770044e87531bdf341f0a507bf
soorajjr1951-dev <soorajjr1951@gmail.com>
2026-01-28 14:24:14 +0530

    Fix CORS for Netlify frontend

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,8 +1,34 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+        },
+      },
+    },
+  },
+
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000',          // local frontend
+        'http://127.0.0.1:3000',
+        'https://westernbeachventures.netlify.app', // Netlify
+      ],
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    },
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
